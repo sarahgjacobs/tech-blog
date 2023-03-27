51b7073c7cf28ae4ecb63919d6320b52494ad842
Sarah Jacobs <sgjacobs14@gmail.com>
2023-03-27 19:25:03 -0400

    updated routes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
 
 
